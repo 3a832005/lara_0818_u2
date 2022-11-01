44f633641064a149a0372856dcdeb2cf0a419238
李晏慈 <3a832005@gm.student.ncut.edu.tw>
2022-11-01 23:45:22 +0800

    練習1-6:修改route載入的頁面路徑

--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
+/*練習1-2
 Route::get('home',function(){
     return view('home');
 });
+*/
+//練習1-6
+Route::get('home',function(){
+    return view('home.index');
+});
+
 
