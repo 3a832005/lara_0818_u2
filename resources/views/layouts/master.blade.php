dad1d43547a63c248660f1fe5bc7a0f95dca2487
李晏慈 <3a832005@gm.student.ncut.edu.tw>
2022-11-02 00:06:51 +0800

    練習2-1~2-2:建立主樣版與子樣版

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>@yield('title')|MyBlog</title>
+</head>
+<body>
+    <divclass="container">
+        @yield('content')
+    </div>
+</body>
+</html>
